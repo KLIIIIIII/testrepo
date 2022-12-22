65414f4a6d5870a6027dc2c5afbf1d888ffe3c8d
KLIIIIIII <63213890+KLIIIIIII@users.noreply.github.com>
2022-12-21 22:51:59 -0500

    Create quick_calc.py

---
+++ quick_calc.py
@@ -0,0 +1,8 @@
+# create a simple calculation and merge it into the master branch
+import numpy as np
+import pandas as pd
+
+list_of_b = [5, 2, 8, 20, 4, 5]
+
+b = pd.DataFrame(list_of_b)
+print(b)
